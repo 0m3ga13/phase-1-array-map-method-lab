926c80cfddf751e0cf25ed74e0a9b668e8796258
0m3ga13 <barkaoussama13@gmail.com>
2023-08-05 18:34:11 +0100

    Done

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
+
+
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(str => str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' '))
 }
+
